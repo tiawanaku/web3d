5afa56fc85882f61bdbcbc4ec3e85eebe6978bf2
alanezfranz <alanezfranz@gmail.com>
2024-05-19 17:46:00 -0400

    prototipo de web 3d

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { House } from "./components/House/House";
+import { OrbitControls, Sky } from "@react-three/drei";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ width: "100%", height: "80vh" }}>
+        <Canvas camera={{ zoom: 1, position: [15, 20, 15] }}>
+          <ambientLight intensity={0.5} />
+          <pointLight position={[35, 35, 0]} intensity={0.4} />
+          <pointLight position={[-35, 35, 0]} intensity={0.4} />
+          <Sky
+            distance={450000}
+            sunPosition={[0, 1, 0]}
+            inclination={0}
+            azimuth={0.25}
+          />
+          <Suspense fallback={null}>
+            <House />
+          </Suspense>
+          <OrbitControls />
+        </Canvas>
+      </div>
+    </>
   );
 }
 
